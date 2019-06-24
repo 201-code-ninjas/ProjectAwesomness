f912ee114e1fbc4f09fa7fd612dec3691a70e214
messickr <rnmessick@gmail.com>
2019-06-24 16:22:21 -0700

    more gamepage canvas functions

--- js/gamePage.js
+++ js/gamePage.js
@@ -3,3 +3,12 @@ var ctx = circleScore.getContext('2d');
 ctx.beginPath();
 ctx.arc(100, 75, 50, 0, 2 * Math.PI);
 ctx.stroke();
+var img = new Image();
+img.src = '../asset/marisha.png';
+
+window.onload = function(){
+  var canvas = document.getElementById('marishaSprite');
+  var ctx = canvas.getContext('2d');
+
+  ctx.drawImage(img,10, 10);
+};
