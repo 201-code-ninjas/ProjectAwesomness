fe9d334b0ff385e4b35a7130e096ad6da17774da
messickr <rnmessick@gmail.com>
2019-06-25 12:01:25 -0700

    canvas build, hero moves!

--- js/gamePage.js
+++ js/gamePage.js
@@ -5,22 +5,48 @@
 // ctx.beginPath();
 // ctx.arc(100, 75, 50, 0, 2 * Math.PI);
 // ctx.stroke();
+var canvas = document.getElementById('gamePageCanvas');
+var ctx = canvas.getContext('2d');
+
 
 var groundLevelImg = new Image();
-groundLevelImg.src = '../assets/scenery-ground.png';
-
 var wallOfDeathGif = new Image();
-wallOfDeathGif.src = '../assets/wallOfDeath.png';
+var heroImage = new Image();
+var wallOfWinImg = new Image();
 
 var canvasWidth = 960;
 var canvasHeight = 500;
 var groundLevel = canvasHeight - 174;
-// console.log(typeof groundLevelImg);
+var heroXPosition = canvasWidth/2;
 
-window.onload = function(){
-  var canvas = document.getElementById('gamePageCanvas');
-  var ctx = canvas.getContext('2d');
+function moveHero(){
+  heroXPosition +=10;
+  
+}
+
+function init(){
+
+  groundLevelImg.src = '../assets/scenery-ground.png';
+  wallOfDeathGif.src = '../assets/wallOfDeath.png';
+  heroImage.src = '../assets/hero.png';
+  wallOfWinImg.src = '../assets/wallOfWin.png';
+
+  window.requestAnimationFrame(draw);
+}
+
+function draw(){
+  ctx.fillStyle = '#44a8fe';
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
   ctx.drawImage(groundLevelImg, 0, groundLevel);
   ctx.drawImage(wallOfDeathGif, 0, groundLevel-300);
-};
+  ctx.drawImage(heroImage, heroXPosition, groundLevel-126);
+  ctx.drawImage(wallOfWinImg, canvasWidth-33, groundLevel-247);
+
+  window.requestAnimationFrame(draw);
+}
+
+init();
+
+
+
