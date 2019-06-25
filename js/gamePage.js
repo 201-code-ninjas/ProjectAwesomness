64de69a6903c33e9183a9e1cd5fb9b4d44b96e23
messickr <rnmessick@gmail.com>
2019-06-25 11:03:29 -0700

    canvas image functions

--- js/gamePage.js
+++ js/gamePage.js
@@ -1,14 +1,24 @@
-var circleScore = document.getElementById('scoreKeeper');
-var ctx = circleScore.getContext('2d');
-ctx.beginPath();
-ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-ctx.stroke();
-var img = new Image();
-img.src = '../assets/scenery-ground.png';
+// var circleScore = document.getElementById('scoreKeeper');
+// var ctx = circleScore.getContext('2d');
+// ctx.beginPath();
+// ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+// ctx.stroke();
+
+var groundLevelImg = new Image();
+groundLevelImg = '../assets/scenery-ground.png';
+
+// var wallOfDeathGif = new Image();
+// wallOfDeathGif = '../assets/wallOfDeath.png';
+
+var canvasWidth = 960;
+var canvasHeight = 500;
+var groundLevel = canvasHeight - 174;
+// console.log(typeof groundLevelImg);
 
 window.onload = function(){
-  var canvas = document.getElementById('sceneryGroundImage');
+  var canvas = document.getElementById('gamePageCanvas');
   var ctx = canvas.getContext('2d');
 
-  ctx.drawImage(img,105, 0);
+  ctx.drawImage(groundLevelImg, 0, groundLevel);
+  // ctx.drawImage(wallOfDeathGif, 0, 0);
 };
