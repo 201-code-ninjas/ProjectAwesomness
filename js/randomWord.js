bacf8dcbbf6a0ef19f3d92a25d4da73377314e57
cmcoulon <ccoulon@amazon.com>
2019-06-27 13:30:04 -0700

    Thursday morning group work

--- js/randomWord.js
+++ js/randomWord.js
@@ -7,6 +7,7 @@ function getRandomWord(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
 //function to randomly pickup word from easy word list 
 var pickFromEasyWordList = function () {
   var index, easyWord;
